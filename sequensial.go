c5f0aca763bbe63a21477c3848c6ecfec84bbf1f
wibu-elite <miftahurrohman100@gmail.com>
2022-07-12 16:51:35 +0700

    pak amal

--- sequensial.go
+++ sequensial.go
@@ -2,25 +2,29 @@ package main
 
 import (
 	"fmt"
+	"strings"
 	// "strings"
 )
 
 func main() {
 	fmt.Printf("\x1bc")
 	fmt.Println("PROGRAM SEDERHANA SEARCHING SEQUENSIAL MIFTA N R 10", "\n")
-	fmt.Println(`List Data :`,"\n",
-	`1.  Januari`,"\n",
-	`2.  Februari`,"\n",
-	`3.  Maret`,"\n",
-	`4.  April`,"\n",
-	`5.  Mei`,"\n",
-	`6.  Juni`,"\n",
-	`7.  Juli`,"\n",
-	`8.  Agustus`,"\n",
-	`9.  September`,"\n",
-	`10. Oktober`,"\n",
-	`11. November`,"\n",
-	`12. Desember`,"\n")	
+	text := `
+List Data :                                           
+1.  Januari                                          
+2.  Februari
+3.  Maret
+4.  April
+5.  Mei
+6.  Juni
+7.  Juli
+8.  Agustus
+9.  September
+10. Oktober
+11. November
+12. Desember
+	`
+	fmt.Println(text)
 
 	var (
 		month = [12]string{"Januari", "Februari", "Maret", "April", "Mei", "Juni", "Juli",
@@ -32,21 +36,25 @@ func main() {
 	fmt.Scanln(&search)
 
 	// search := strings.ToLower(search)
-	while(true){
-		
-	}
-	for i := 0; i < 12; i++ {
+	// while(true){
+
+	// }
+	for i := 0; i < len(month); i++ {
 		// search := strings.ToLower(search)
-		if month[i] == search {
+		if strings.EqualFold(month[i], search) {
 			fmt.Println("Data Ditemukan Pada Index Ke: ", i)
 			return
 		}
-	}
-	for i := 0; i < 12; i++ {
-		// search := strings.ToLower(search)
-		if month[i] != search {
+		if i == (len(month) - 1) {
 			fmt.Println("Tidak Ada Data Ditemukan")
-			return
+			break
 		}
 	}
+	// for i := 0; i < 12; i++ {
+	// 	// search := strings.ToLower(search)
+	// 	if month[i] != search {
+	// 		fmt.Println("Tidak Ada Data Ditemukan")
+	// 		return
+	// 	}
+	// }
 }
