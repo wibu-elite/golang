4497b9bf9b17fd48945d78fce1a0a6d3b8e6e0c5
wibu-elite <miftahurrohman100@gmail.com>
2022-07-15 14:48:49 +0700

    Binary done

--- binary2.go
+++ binary2.go
@@ -4,23 +4,24 @@ import (
 	"fmt"
 )
 
-var (
-	banyakData int
-	urutkan    int
-	cariData   int
-	Hasil      int
-)
-
+var ()
 
 // Fungsi Mengurutkan Bilangan
-func BinarySearch(banyakData int, urutkan int, cariData int) {
+func main() {
+	fmt.Printf("\x1bc")
+	var (
+		urut       int
+		value      = make([]int, 254)
+		banyakData int
+		mid        int
+		cariData   int
+		// i = 0
+		// Hasil      int
+	)
 	fmt.Printf("Masukkan Bnayak Data: ")
 	fmt.Scanln(&banyakData)
-
-	var urut int
-	var value = make([]int, 254)
 	for i := 0; i < banyakData; i++ {
-		fmt.Print("Masukkan Data Ke", i+1,": ")
+		fmt.Print("Masukkan Data Ke", i+1, ": ")
 		fmt.Scanln(&value[i])
 	}
 
@@ -41,26 +42,28 @@ Hasil Data Setelah Diurutkan: `)
 	fmt.Printf(`
 Masukkan Data yang Ingin Dicari: `)
 	fmt.Scanln(&cariData)
-	fmt.Println(cariData)
 
-	for i := 0; i < len(value); i++{
-		low := 0
-		high := len(value)
-		mid := (low + high) / 2
+	low := 0
+	high := banyakData
+	// fmt.Println(high)
+	for low <= high{
+		mid = (high +low)/2
 		if cariData < value[mid] {
 			high = mid - 1
-		} else if cariData > value[mid] {
-			high = mid + 1
-		} else if cariData == value[mid] {
-			
+		} 
+		if cariData > value[mid] {
+			low = mid + 1
+		}
+		if value[mid] == cariData{
+			fmt.Print("Data Ditemukan Pada Index Ke: ", mid-1)
+			return
 		}
 	}
-}
-func main() {
-	var banyakData int
-	fmt.Printf("\x1bc")
-	fmt.Println(`Program Binary Searching Dengan Inputan Dari User
-	`)
 
-	BinarySearch(banyakData, urutkan, cariData)
+
+	// for i := 0; i < banyakData; i++{
+	// 	if value[mid] == cariData{
+	// 		fmt.Print("Data", cariData, "Ditemukan Pada ", mid) 
+	// 	}
+	// }
 }
