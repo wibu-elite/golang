17c4e4ecc683f24f6719cfc1b5e958982b23ce1e
wibu-elite <miftahurrohman100@gmail.com>
2022-07-21 22:03:16 +0700

    add function-param

--- function-param.go
+++ function-param.go
@@ -1,22 +1,7 @@
 package main
 
-import (
-	"fmt"
-	"math/rand"
-	"time"
-)
-
-func RandomNumber(){
-	rand.Seed(time.Now().UTC().Unix())
-	
-	for i := 1; i < 5; i++{
-		fmt.Println("Random Number ke", i,":", rand.Intn(7))
-	}
-}
+import "fmt"
 
 func main() {
-	fmt.Print("\x1bc")
-	fmt.Println(`Function With Paramater
-	`)
-	RandomNumber()
+	
 }
